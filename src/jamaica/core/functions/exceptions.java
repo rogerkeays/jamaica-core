4afc1dde9e20915cc6bc4c76b978d0bcb3c5918d
Roger Keays <roger.keays@ninthavenue.com.au>
2016-11-27 22:25:17 +0530

    move error formatting to exceptions library

--- src/jamaica/core/functions/exceptions.java
+++ src/jamaica/core/functions/exceptions.java
@@ -3,10 +3,14 @@ package jamaica.core.functions;
 import java.io.*;
 import java.util.*;
 import jamaica.core.exceptions.*;
+import jamaica.core.interfaces.*;
 import jamaica.core.types.*;
 import org.testng.annotations.*;
 import static jamaica.core.functions.collections.*;
+import static jamaica.core.functions.i18n.*;
+import static jamaica.core.functions.lang.*;
 import static jamaica.core.functions.testing.*;
+import static java.text.MessageFormat.*;
 import static org.testng.Assert.*;
 
 public class exceptions {
@@ -23,6 +27,39 @@ public class exceptions {
         Tuple<Exception, Integer> tuple = new Tuple<>(one, two);
         errors.list.add(tuple);
         return tuple;
+    }
+
+
+    // format_line_errors
+    @Test public void format_line_errors__creates_a_single_string_summarising_the_errors() {
+        ExceptionTuples errors = new ExceptionTuples();
+        add_tuple(errors, new NumberFormatException("foo"), 5);
+        assertEquals("Line 5: NumberFormatException: foo\n", format_line_errors(errors));
+    }
+    @Test public void format_line_errors__accepts_a_function_for_localising_each_line() {
+        ExceptionTuples errors = new ExceptionTuples();
+        add_tuple(errors, new NumberFormatException("foo"), 5);
+        add_tuple(errors, new NumberFormatException("bar"), 6);
+        assertEquals("Line 5: FOO\nLine 6: BAR\n", 
+                format_line_errors(errors, e -> e.getMessage().toUpperCase()));
+    }
+    @Test public void format_line_errors__falls_back_to_default_localisation_if_localisation_function_returns_null() {
+        ExceptionTuples errors = new ExceptionTuples();
+        add_tuple(errors, new NumberFormatException("foo"), 5);
+        assertEquals("Line 5: NumberFormatException: foo\n", format_line_errors(errors, e -> null));
+    }
+    public static String format_line_errors(ExceptionTuples errors) {
+        return format_line_errors(errors, null);
+    }
+    public static String format_line_errors(ExceptionTuples errors, Function<Throwable, String> localisation_function) {
+        final StringBuilder result = new StringBuilder();
+        for (Tuple<Exception, Integer> tuple : errors.list) {
+            result.append(format(localise("Line {0}: "), tuple.two))
+                .append(localisation_function == null ? localise_exception(tuple.one) :
+                    coalesce(localisation_function.apply(tuple.one), localise_exception(tuple.one)))
+                .append("\n");
+        }
+        return result.toString();
     }
 
 

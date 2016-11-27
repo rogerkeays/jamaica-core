090b6a78a7ed3bbaf06201720816146d7b7aded9
Roger Keays <roger.keays@ninthavenue.com.au>
2016-11-27 20:14:25 +0530

    remove testng asserts

--- src/jamaica/core/functions/io.java
+++ src/jamaica/core/functions/io.java
@@ -2,24 +2,15 @@ package jamaica.core.functions;
 
 import java.io.*;
 import java.util.*;
+import jamaica.core.exceptions.*;
+import jamaica.core.interfaces.*;
 import org.testng.annotations.*;
 import static jamaica.core.functions.exceptions.*;
 import static jamaica.core.functions.lang.*;
 import static jamaica.core.functions.strings.*;
-import static org.testng.Assert.*;
+import static jamaica.core.functions.testing.*;
 
 public class io {
-
-    // create_test_filename
-    @Test public void create_test_filename__uses_the_target_directory() {
-        assertTrue(create_test_filename().startsWith("target/"));
-    }
-    @Test public void create_test_filename__creates_unique_filenames() {
-        assertNotEquals(create_test_filename(), create_test_filename());
-    }
-    public static String create_test_filename() {
-        sleep(1); return "target/test-" + System.currentTimeMillis();
-    }
 
 
     // get_lines
@@ -28,32 +19,32 @@ public class io {
         get_lines("this file doesn't exist");
     }
     @Test public void get_lines__parses_an_empty_file_to_an_empty_array() {
-        assertEquals(get_lines(write_tmp_file("")).length, 0);
+        assert_equals(get_lines(write_tmp_file("")).length, 0);
     }
     @Test public void get_lines__parses_a_single_line_file_to_a_one_element_array() {
         File tmp = write_tmp_file("Hello");
         String[] lines = get_lines(tmp);
-        assertEquals(lines.length, 1);
-        assertEquals(lines[0], "Hello");
+        assert_equals(lines.length, 1);
+        assert_equals(lines[0], "Hello");
     }
     @Test public void get_lines__parses_a_multi_line_file_to_a_multi_element_array() {
         File tmp = write_tmp_file("Hello\nWorld");
         String[] lines = get_lines(tmp);
-        assertEquals(lines.length, 2);
-        assertEquals(lines[0], "Hello");
-        assertEquals(lines[1], "World");
+        assert_equals(lines.length, 2);
+        assert_equals(lines[0], "Hello");
+        assert_equals(lines[1], "World");
     }
     @Test public void get_lines__parses_windows_line_feeds() {
         File tmp = write_tmp_file("Hello\r\nWorld");
         String[] lines = get_lines(tmp);
-        assertEquals(lines.length, 2);
-        assertEquals(lines[0], "Hello");
-        assertEquals(lines[1], "World");
+        assert_equals(lines.length, 2);
+        assert_equals(lines[0], "Hello");
+        assert_equals(lines[1], "World");
     }
     @Test public void get_lines__ignores_file_empty_lines() {
         File tmp = write_tmp_file("Hello\nWorld\n");
         String[] lines = get_lines(tmp);
-        assertEquals(lines.length, 2);
+        assert_equals(lines.length, 2);
     }
     public static String[] get_lines(String path) {
         return get_lines(new File(path));
@@ -66,13 +57,13 @@ public class io {
 
     // read_text_file
     @Test public void read_text_file__can_read_a_text_file_from_tmp_dir() {
-        assertEquals(read_text_file(write_tmp_file("Hello World")), "Hello World");
+        assert_equals(read_text_file(write_tmp_file("Hello World")), "Hello World");
     }
     @Test public void read_text_file__parses_an_empty_file_into_an_empty_string() {
-        assertEquals(read_text_file(write_tmp_file("")), "");
+        assert_equals(read_text_file(write_tmp_file("")), "");
     }
     @Test public void read_text_file__reads_long_files_in_their_entirety() {
-        assertEquals(read_text_file(write_tmp_file(new String(new char[129000]))).length(), 129000);
+        assert_equals(read_text_file(write_tmp_file(new String(new char[129000]))).length(), 129000);
     }
     public static String read_text_file(String filename) {
         return read_text_file(new File(filename));
@@ -85,17 +76,16 @@ public class io {
             stream.readFully(bytes);
             return new String(bytes, "UTF-8");
         } catch (Exception e) {
-            throw_checked(e); 
+            throw checked(e); 
         } finally {
             if (stream != null) {
                 try {
                     stream.close();
                 } catch (IOException e) {
-                    throw_checked(e);
+                    throw checked(e);
                 }
             }
         }
-        return null;
     }
 
 
@@ -106,11 +96,11 @@ public class io {
     }
     @Test public void write_text_file__creates_a_file_on_disk() {
         File file = write_text_file(create_test_filename(), "Hello World");
-        assertNotNull(file);
-        assertTrue(file.exists());
+        assert_true(file != null);
+        assert_true(file.exists());
     }
     @Test public void write_text_file__writes_the_value_passed_as_a_parameter() {
-        assertEquals(read_text_file(write_text_file(create_test_filename(), "Hello World")), "Hello World");
+        assert_equals(read_text_file(write_text_file(create_test_filename(), "Hello World")), "Hello World");
     }
     public static File write_text_file(String file, String content) {
         return write_text_file(new File(file), content);
@@ -122,14 +112,13 @@ public class io {
             writer.write(content);
             return file;
         } catch (IOException e) {
-            throw_checked(e);
-            return null;
+            throw checked(e);
         } finally {
             if (writer != null) {
                 try {
                     writer.close();
                 } catch (IOException e) {
-                    throw_checked(e);
+                    throw checked(e);
                 }
             }
         }
@@ -139,11 +128,11 @@ public class io {
     // write_tmp_file
     @Test public void write_tmp_file__creates_a_file_on_disk() {
         File tmp = write_tmp_file("Hello World");
-        assertNotNull(tmp);
-        assertTrue(tmp.exists());
+        assert_true(tmp != null);
+        assert_true(tmp.exists());
     }
     @Test public void write_tmp_file__writes_the_value_passed_as_a_parameter() {
-        assertEquals(read_text_file(write_tmp_file("Hello World")), "Hello World");
+        assert_equals(read_text_file(write_tmp_file("Hello World")), "Hello World");
     }
     public static File write_tmp_file(String content) {
         try {
@@ -151,8 +140,7 @@ public class io {
             tmp.deleteOnExit();
             return write_text_file(tmp, content);
         } catch (IOException e) {
-            throw_checked(e);
-            return null;
+            throw checked(e);
         }
     }
 }

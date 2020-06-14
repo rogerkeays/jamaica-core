076b1c6ba8cdfd0c946c7679c6d0bf8f7000abcf
Roger Keays <roger.keays@ninthavenue.com.au>
2020-06-14 15:24:27 +0700

    add contracts library for writing specs as tests

---
+++ src/contracts.java
@@ -0,0 +1,8 @@
+package jamaica.core;
+
+import org.testng.annotations.*;
+import static org.testng.AssertJUnit.*;
+
+public class contracts {
+    public static void eq(Object o1, Object o2) { assertEquals(o1, o2); }
+}
